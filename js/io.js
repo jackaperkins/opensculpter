f950552746a29424c5f20485db7a4ba903e948a7
Jack Perkins <jackaperkins@gmail.com>
2014-05-26 21:23:48 -0700

    cleanup and some commenting

--- js/io.js
+++ js/io.js
@@ -1,3 +1,7 @@
+/**
+ * just a plce for detectMain.js to copy it's data for others to read
+ */
+
 var io = {
     coords: [0,0,0,0]
 };
--- js/renderer.js
+++ js/renderer.js
@@ -1,83 +1,95 @@
-
-$(function() {
-var scene = new THREE.Scene();
-
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-var renderer = new THREE.WebGLRenderer( { alpha: true } );
-renderer.setSize( 640, 480);
-$('#canvas-holder').append( renderer.domElement );
+/**
+ * This is the script where we render the 3d objects using the dect data
+ */
 
 
-var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-directionalLight.position.set( 0, 1, 0 );
-scene.add( directionalLight );
+//this dollarsign wrapping a nameless function is all
+// jquery making sure none of this runs till the page is ready
+$(function () {
+    var scene = new THREE.Scene();
 
-var geometry = new THREE.SphereGeometry(2,.15,.15);
-var material = new THREE.MeshPhongMaterial( { ambient: 0x444444, color: 0xA3585B, specular: 0x49D8FB, shininess: 80, perPixel: false, overdraw: true } );
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-var original = [];
-
-for(var i=0; i< cube.geometry.vertices.length; i++){
-    original.push({
-        x: cube.geometry.vertices[i].x,
-        y: cube.geometry.vertices[i].y,
-        z: cube.geometry.vertices[i].z,
+    var renderer = new THREE.WebGLRenderer({
+        alpha: true //alpha means the background is by default transparent so we can see webcam
     });
-}
 
-pointLight = new THREE.PointLight( 0xffffff, .8 );
-//pointLight.lookAt(mesh)
-pointLight.position = camera.position;
+    // add the rendering element (canvas) to the DOM
+    renderer.setSize(640, 480);
+    $('#canvas-holder').append(renderer.domElement);
 
-//pointLight.castShadow = true;
-scene.add( pointLight );
+    var directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    directionalLight.position.set(0, 1, 0);
+    scene.add(directionalLight);
 
+    // creating our sphere and it's material
+    var geometry = new THREE.SphereGeometry(1);
+    var material = new THREE.MeshPhongMaterial({
+        ambient: 0x444444,
+        color: 0xA3585B,
+        specular: 0x94D5B,
+        shininess: 10,
+        perPixel: false,
+        overdraw: true
+    });
 
-camera.position.z = 5;
-render();
+    // actually it's a sphere, but the variable name is cube [ha ha ha]
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 
+    var original = [];
 
-function render() {
-    //in your update/draw function
-    if(io.coords){
-        cube.position.x = 0.013*io.coords[0]-3;
-        cube.position.y = -0.01*io.coords[1]+1;
+    for (var i = 0; i < cube.geometry.vertices.length; i++) {
+        original.push({
+            x: cube.geometry.vertices[i].x,
+            y: cube.geometry.vertices[i].y,
+            z: cube.geometry.vertices[i].z,
+        });
     }
 
-    cube.rotation.y += 0.008;
-    cube.rotation.z += 0.007;
-    distort(cube);
+    pointLight = new THREE.PointLight(0xffffff, .8);
+    //pointLight.lookAt(mesh)
+    pointLight.position = camera.position;
 
-    //cube.rotateOnAxis(axis, 0.05);
+    //pointLight.castShadow = true;
+    scene.add(pointLight);
 
-    requestAnimationFrame(render);
-    renderer.render(scene, camera);
-}
+    camera.position.z = 5;
+    render();
 
-function distort(object) {
-    var factor = .35 * 0.01*io.coords[0];
-    factor *= factor;
+    function render() {
+        //in your update/draw function
+        if (io.coords) {
+            cube.position.x = 0.015 * io.coords[0] - 3.4;
+            cube.position.y = -0.01 * io.coords[1] + 1;
+        }
 
-   // object.geometry.vertices = temp;
+        cube.rotation.y += 0.008;
+        cube.rotation.z += 0.007;
+        distort(cube);
 
-    var  maxVertices = object.geometry.vertices.length;
+        //cube.rotateOnAxis(axis, 0.05);
 
-    for (var i = 0; i < maxVertices; i++) {
-        var vertex = object.geometry.vertices[i];
-
-        vertex.x =  original[i].x + ( Math.random(1) - 0.5 ) * factor;
-        vertex.y  = original[i].y + ( Math.random(1) - 0.5 ) * factor;
-        vertex.z  = original[i].z + ( Math.random(1) - 0.5 ) * factor;
+        requestAnimationFrame(render);
+        renderer.render(scene, camera);
     }
 
-    object.geometry.dynamic = true;
-    object.geometry.verticesNeedUpdate = true;
-}
+    function distort(object) {
+        var factor = .1 * 0.02 * io.coords[0];
+        factor *= factor;
+
+        var maxVertices = object.geometry.vertices.length;
+
+        for (var i = 0; i < maxVertices; i++) {
+            var vertex = object.geometry.vertices[i];
+
+            vertex.x += (original[i].x - vertex.x) * 0.3 + (Math.random(1) - 0.5) * factor;
+            vertex.y += (original[i].y - vertex.y) * 0.3 + (Math.random(1) - 0.5) * factor;
+            vertex.z += (original[i].z - vertex.z) * 0.3 + (Math.random(1) - 0.5) * factor;
+        }
+
+        object.geometry.dynamic = true;
+        object.geometry.verticesNeedUpdate = true;
+    }
 
 });
-
-
